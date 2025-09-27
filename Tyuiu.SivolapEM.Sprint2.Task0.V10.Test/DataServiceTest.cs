837306c7f159e89202841992d75a6ec0f5414477
Tyuiu.SivolapEM@outlook.com <Tyuiu.SivolapEM@outlook.com>
2025-09-27 20:43:04 +0500

    Add project files.

---
+++ Tyuiu.SivolapEM.Sprint2.Task0.V10.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.SivolapEM.Sprint2.Task0.V10.Lib;
+
+namespace Tyuiu.SivolapEM.Sprint2.Task0.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExperessions()
+        {
+            DataService ds = new DataService();
+            bool[] res = new bool[6];
+            res[0] = false;
+            res[1] = true;
+            res[2] = false;
+            res[3] = true;
+            res[4] = false;
+            res[5] = true;
+            CollectionAssert.AreEqual(res, ds.GetCompareOperations(1305, 475));
+        }
+    }
+}
---
+++ Tyuiu.SivolapEM.Sprint2.Task0.V10/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.SivolapEM.Sprint2.Task0.V10.Lib;
+
+namespace Tyuiu.SivolapEM.Sprint2.Task0.V10
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            bool[] res = ds.GetCompareOperations(1305, 475);
+            int x, y;
+            x = 1305;
+            y = 475;
+
+            Console.Title = "Спринт #2 | Выполнил: Сиволап Егор Максимович | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Сиволап Егор Максимович | ИИПб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений,                               *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив)                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* X = " + x);
+            Console.WriteLine("* Y = " + y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+        }
+    }
+}
