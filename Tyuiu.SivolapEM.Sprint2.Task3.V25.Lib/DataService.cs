9515d32ac4216f940a00bcca70fb408a8587459d
Tyuiu.SivolapEM@outlook.com <Tyuiu.SivolapEM@outlook.com>
2025-09-29 21:57:23 +0500

    Task3 upd2

--- Tyuiu.SivolapEM.Sprint2.Task3.V25.Lib/DataService.cs
+++ Tyuiu.SivolapEM.Sprint2.Task3.V25.Lib/DataService.cs
@@ -10,20 +10,20 @@ namespace Tyuiu.SivolapEM.Sprint2.Task3.V25.Lib
             {
                 double sinPart = Math.Sin(Math.Pow(x, 3));
                 double fractionPart = Math.Pow((x + 1) / (x - 1), x);
-                return sinPart + fractionPart;
+                return Math.Round(sinPart + fractionPart, 3);
             }
             else if (x == 0)
             {
                 double cosPart = Math.Cos(Math.Pow(x, 3));
-                return Math.Pow(x, 2) - cosPart / (x - 3);
+                return Math.Round(Math.Pow(x, 2) - cosPart / (x - 3), 3);
             }
             else if (x > -29 && x < 2)
             {
-                return Math.Pow(1 + 1 / Math.Pow(x, 2), x);
+                return Math.Round(Math.Pow(1 + 1 / Math.Pow(x, 2), x), 3);
             }
             else if (x < -29)
             {
-                return x + 15 + (12 / x) * x;
+                return Math.Round(x + 15 + (12 / x) * x, 3);
             }
             else return 0;
         }
